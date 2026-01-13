2eab12233272c2fab4e6e0ac0b0235ad9b3ab45a
ohuanasis <ohuanasis@gmail.com>
2026-01-13 17:19:22 -0500

    added nuralnetworkproject

---
+++ AIConceptsNET/NeuralNetwork/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace NeuralNetwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            
+            NeuralNetwork neuralNetwork = new NeuralNetwork();
+        }
+    }
+
+    internal class NeuralNetwork
+    {
+        public NeuralNetwork()
+        {
+            // Initialize neural network
+        }
+    }
+}
