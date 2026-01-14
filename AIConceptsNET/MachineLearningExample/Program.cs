8bc7a1d734537fe887d193fb52a3684a7e69f7c4
ohuanasis <ohuanasis@gmail.com>
2026-01-14 16:32:24 -0500

    added csv file

--- AIConceptsNET/MachineLearningExample/Program.cs
+++ AIConceptsNET/MachineLearningExample/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace MachineLearningExample
+﻿using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace MachineLearningExample
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MLContext mlContext = new MLContext();
+
+            IDataView data = mlContext.Data.LoadFromTextFile<HousingData>("housing-data.csv", separatorChar:',', hasHeader:true);
+
+            string[] featureColumns = { "SquareFeet", "Bedrooms" };
+            string labelColumn = "Price";
+
+            var pipeline = mlContext.Transforms.Concatenate("Features", featureColumns)
+                .Append(mlContext.Regression.Trainers.FastTree(labelColumnName: labelColumn));
+
+            var model = pipeline.Fit(data);
+            var prediction = model.Transform(data);
+            var metrics = mlContext.Regression.Evaluate(prediction, labelColumnName: labelColumn);
+            Console.WriteLine($"Mean Absolute Error: {metrics.MeanAbsoluteError}");
+            Console.WriteLine($"Root Mean Squared Error: {metrics.RootMeanSquaredError}");
         }
+    }
+
+    internal class HousingData
+    {
+        [LoadColumn(0)]
+        public float SquareFeet { get; set; }
+        [LoadColumn(1)]
+        public float Bedrooms { get; set; }
+        [LoadColumn(2)]
+        public float Price { get; set; }
+
+    }
+
+    internal class HousingPrediction
+    {
+        [ColumnName("Score")]
+        public float Price { get; set; }
     }
 }
