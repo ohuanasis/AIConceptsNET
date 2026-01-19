35906038eff35f5ffec172e68751e91994219eb9
ohuanasis <ohuanasis@gmail.com>
2026-01-18 22:41:32 -0500

    added new class

---
+++ AIConceptsNET/SentimentAnalysis/Classification/MovieReview.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML.Data;
+
+namespace SentimentAnalysis.Classification
+{
+    internal class MovieReview
+    {
+        [LoadColumn(0)]
+        public string text { get; set; }
+        [LoadColumn(1), ColumnName("Label")]
+        public bool sentiment { get; set; }
+    }
+}
--- AIConceptsNET/SentimentAnalysis/Program.cs
+++ AIConceptsNET/SentimentAnalysis/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace SentimentAnalysis
+﻿using Microsoft.ML;
+using SentimentAnalysis.Classification;
+
+namespace SentimentAnalysis
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MLContext mlContext = new MLContext();
+
+            string dataPath = "movieReviewsTraining.csv";
+            string text = File.ReadAllText(dataPath);
+
+            using (StreamReader sr = new StreamReader(dataPath)) 
+            {
+                text = text.Replace("\'", "");
+            }
+
+            File.WriteAllText(dataPath, text);
+
+            IDataView dataView = mlContext.Data.LoadFromTextFile<MovieReview>(dataPath, hasHeader: true, allowQuoting: true, separatorChar: ',');
+
+            var preview = dataView.Preview();
+
+            foreach (var row in preview.RowView)
+            {
+                Console.WriteLine($"{row.Values[0]} | {row.Values[1]}");
+            }
+
         }
     }
+
 }
