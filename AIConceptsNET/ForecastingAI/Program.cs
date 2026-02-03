69e7df188b7f28062a47b40f94c6d31a00e02602
ohuanasis <ohuanasis@gmail.com>
2026-02-03 10:25:08 -0500

    added new porject

---
+++ AIConceptsNET/ForecastingAI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ForecastingAI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
